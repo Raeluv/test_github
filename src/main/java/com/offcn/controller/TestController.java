133d42685d763b71f89afab083c7da9a8bb727d5
zhucan <raeluv233@outlook.com>
2020-11-30 17:37:22 +0800

    测试提交修改后的代码

--- src/main/java/com/offcn/controller/TestController.java
+++ src/main/java/com/offcn/controller/TestController.java
@@ -1,7 +1,10 @@
 package com.offcn.controller;
 
+//分享本地项目到GitHub上：VSS ->import into version control ->share project to GitHub
 public class TestController {
     public void githubTest(){
+        //提交代码到本地仓库 在修改的位置右键 -> Git -> commit ***
+        //提交本地修改后的代码到GitHub
         System.out.println("本地修改，提交到GitHub");
     }
 }
