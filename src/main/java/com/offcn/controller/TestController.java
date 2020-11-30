a2a0e2d4e42f5e8ae488c44e323cf4c96686ee61
zhucan <raeluv233@outlook.com>
2020-11-30 18:08:47 +0800

    测试提交修改后的代码

--- src/main/java/com/offcn/controller/TestController.java
+++ src/main/java/com/offcn/controller/TestController.java
@@ -6,6 +6,7 @@ public class TestController {
         //提交代码到本地仓库 在修改的位置右键 -> Git -> commit *** -> commit（仅提交到本地仓库） / commit and push（提交到本地仓库和中央仓库GitHub）
         //提交本地修改后的代码到GitHub：右键 -> Git -> repository -> push（将本地仓库中的代码修改，提交到中央仓库GitHub）
         System.out.println("本地修改，提交到GitHub");
+        //下载GitHub中修改后的代码：右键 -> Git -> repository -> pull
         System.out.println("xxx修改的代码，提交到GitHub");
     }
 }
